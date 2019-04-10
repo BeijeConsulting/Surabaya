dee567518dddf4950362ae50ab04ed46f87a8869
ivfly <ivo.mosca@privategriffe.com>
2019-04-10 17:22:56 +0200

    appendInFile

--- src/it/beije/surabaya/esempi/CSVutils.java
+++ src/it/beije/surabaya/esempi/CSVutils.java
@@ -1,8 +1,10 @@
 package it.beije.surabaya.esempi;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,55 +15,82 @@ import java.util.StringTokenizer;
 
 public class CSVutils {
 	
-	public static List<String> getFileAsStrings(String pathFile) throws Exception {
-		List<String> content = new ArrayList<String>();
-		
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new FileReader(pathFile));
-			
+//	public static List<String> getFileAsStrings(String pathFile) throws Exception {
+//		List<String> content = new ArrayList<String>();
+//		
+//		BufferedReader reader = null;
+//		try {
+//			reader = new BufferedReader(new FileReader(pathFile));
+//			
+//			while(reader.ready()) {
+//				content.add(reader.readLine());
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			throw e;
+//		} finally {
+//			try {
+//				reader.close();
+//			} catch (IOException ioException) {
+//				ioException.printStackTrace();
+//			}
+//		}
+//		
+//		return content;
+//	}
+	
+	public static void appendInFile(String pathFile, String row) throws Exception {
+		File file = new File(pathFile);
+		StringBuilder sb = null;
+		if (file.exists()) {
+			sb = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new FileReader(file));
 			while(reader.ready()) {
-				content.add(reader.readLine());
+				sb.append(reader.readLine());
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		} finally {
-			try {
-				reader.close();
-			} catch (IOException ioException) {
-				ioException.printStackTrace();
-			}
+			reader.close();
 		}
-		
-		return content;
+		BufferedWriter writer = new BufferedWriter(new FileWriter(pathFile));
+		if (sb != null) {
+			writer.write(sb.toString());
+			writer.newLine();
+		}
+		writer.write(row);
+		writer.newLine();
+
+		writer.close();
 	}
 	
 	public static void main (String[] args) throws Exception {
-		File file = new File("C:\\temp\\pippo.txt");
-		//System.out.println(file.exists());		
-		//FileWriter fw = new FileWriter(file);
+		appendInFile("C:\\temp\\pippo2.txt", "pippo2");
 		
-		FileReader fileReader = null;
-		BufferedReader reader = null;
-		try {
-			fileReader = new FileReader(file);
-			reader = new BufferedReader(fileReader);
-			
-			while(reader.ready()) {
-				System.out.println(reader.readLine());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		} finally {
-			try {
-				reader.close();
-				fileReader.close();
-			} catch (IOException ioException) {
-				ioException.printStackTrace();
-			}
-		}
+//		File file = new File("C:\\temp\\pippo.txt");
+//		//System.out.println(file.exists());
+//		//FileWriter fw = new FileWriter(file);
+//		
+//		FileReader fileReader = null;
+//		BufferedReader reader = null;
+//		try {
+//			fileReader = new FileReader(file);
+//			//reader = new BufferedReader(fileReader);
+//			
+////			while(reader.ready()) {
+////				System.out.println(reader.readLine());
+////			}
+//			while(fileReader.ready()) {
+//				System.out.print((char)fileReader.read());
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			throw e;
+//		} finally {
+//			try {
+//				//reader.close();
+//				fileReader.close();
+//			} catch (IOException ioException) {
+//				ioException.printStackTrace();
+//			}
+//		}
 	}
 
 }
