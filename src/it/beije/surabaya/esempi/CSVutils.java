d631f16ca4b0104d948d53fa62288149e432c7ad
ivfly <ivo.mosca@privategriffe.com>
2019-04-11 15:00:27 +0200

    +++

--- src/it/beije/surabaya/esempi/CSVutils.java
+++ src/it/beije/surabaya/esempi/CSVutils.java
@@ -6,91 +6,69 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
+
 
 public class CSVutils {
 	
-//	public static List<String> getFileAsStrings(String pathFile) throws Exception {
-//		List<String> content = new ArrayList<String>();
-//		
-//		BufferedReader reader = null;
-//		try {
-//			reader = new BufferedReader(new FileReader(pathFile));
-//			
-//			while(reader.ready()) {
-//				content.add(reader.readLine());
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			throw e;
-//		} finally {
-//			try {
-//				reader.close();
-//			} catch (IOException ioException) {
-//				ioException.printStackTrace();
-//			}
-//		}
-//		
-//		return content;
-//	}
-	
-	public static void appendInFile(String pathFile, String row) throws Exception {
-		File file = new File(pathFile);
-		StringBuilder sb = null;
-		if (file.exists()) {
-			sb = new StringBuilder();
-			BufferedReader reader = new BufferedReader(new FileReader(file));
+	public static List<String> getFileAsStrings(String pathFile) throws Exception {
+		List<String> content = new ArrayList<String>();
+		
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(pathFile));
+			
 			while(reader.ready()) {
-				sb.append(reader.readLine());
+				content.add(reader.readLine());
 			}
-			reader.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException ioException) {
+				ioException.printStackTrace();
+			}
 		}
+		
+		return content;
+	}
+	
+	public static void writeRowsInFile(String pathFile, String... newRows) throws Exception {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(pathFile));
-		if (sb != null) {
-			writer.write(sb.toString());
+		for (String r : newRows) {
+			writer.write(r);
 			writer.newLine();
 		}
-		writer.write(row);
-		writer.newLine();
+
+		writer.close();
+	}
+
+	public static void appendRowsInFile(String pathFile, String... newRows) throws Exception {
+		File file = new File(pathFile);
+		List<String> oldRows = new ArrayList<String>();
+		if (file.exists()) {
+			oldRows = getFileAsStrings(pathFile);
+//			BufferedReader reader = new BufferedReader(new FileReader(file));
+//			while(reader.ready()) {
+//				oldRows.add(reader.readLine());
+//			}
+//			reader.close();
+		}
+		System.out.println(oldRows);
+		BufferedWriter writer = new BufferedWriter(new FileWriter(pathFile));
+		for (String r : oldRows) {
+			writer.write(r);
+			writer.newLine();
+		}
+		for (String r : newRows) {
+			writer.write(r);
+			writer.newLine();
+		}
 
 		writer.close();
 	}
 	
-	public static void main (String[] args) throws Exception {
-		appendInFile("C:\\temp\\pippo2.txt", "pippo2");
-		
-//		File file = new File("C:\\temp\\pippo.txt");
-//		//System.out.println(file.exists());
-//		//FileWriter fw = new FileWriter(file);
-//		
-//		FileReader fileReader = null;
-//		BufferedReader reader = null;
-//		try {
-//			fileReader = new FileReader(file);
-//			//reader = new BufferedReader(fileReader);
-//			
-////			while(reader.ready()) {
-////				System.out.println(reader.readLine());
-////			}
-//			while(fileReader.ready()) {
-//				System.out.print((char)fileReader.read());
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			throw e;
-//		} finally {
-//			try {
-//				//reader.close();
-//				fileReader.close();
-//			} catch (IOException ioException) {
-//				ioException.printStackTrace();
-//			}
-//		}
-	}
-
 }
---
+++ src/it/beije/surabaya/esempi/Run.java
@@ -0,0 +1,11 @@
+package it.beije.surabaya.esempi;
+
+public class Run {
+
+	public static void main(String[] args) throws Exception {
+		CSVutils.writeRowsInFile("C:\\temp\\pippo2.txt", "pippo1");
+		CSVutils.appendRowsInFile("C:\\temp\\pippo2.txt", "pippo2", "pippo3");
+//		CSVutils.appendRowInFile("C:\\temp\\pippo2.txt", new String[] {"pippo12", "pippo13", "pippo14"});
+	}
+
+}
