1bd89615b9b65311eef425caa8ca01f9e1421cfe
Padawan01 <Padawan01@LAPTOP-8KMGA82P.lan>
2019-04-23 17:16:45 +0200

    Classe di utilità per la creazione di nuovi utenti

---
+++ src/timesheet/TSutils.java
@@ -0,0 +1,28 @@
+package timesheet;
+
+public class TSutils {
+
+	public static boolean creaUtente() {
+
+		boolean esito = false;
+		
+		return esito;
+			
+	}
+
+	public static boolean modificaUtente(String idUtente) {
+
+		boolean esito = false;
+		
+		return esito;
+		
+	}
+
+	public static boolean eliminaUtente(String idUtente) {
+
+		boolean esito = false;
+		
+		return esito;
+	}
+
+}
