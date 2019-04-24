cdf603e06a172b45b0581fb0a1f76fea95f1754e
Padawan01 <Padawan01@LAPTOP-8KMGA82P.lan>
2019-04-24 15:05:17 +0200

    Tsutils commentato e sistemato per i valori null, ancora da modificare per il controllo dei campi identificativi

--- src/timesheet/TSutils.java
+++ src/timesheet/TSutils.java
@@ -1,78 +1,150 @@
 package timesheet;
 import java.util.Scanner;
 public class TSutils {
-
+	static Scanner scan = new Scanner(System.in);
 	public static String creaUtente() {
 		String query=null;
 		
-		Scanner scan = new Scanner(System.in);
+		
 		
 		String nome=null;
 		System.out.println("Nome utente ");
-		nome ="'"+ scan.nextLine()+"'";
+		nome ="'"+ scan.nextLine().trim()+"'";			//Formato per query SQL
 		
-		while (nome.length()==2) {
+		while (nome.length()==2) {						//Se il nome è null, allora resteranno solo gli apici ('')
 			System.out.println("Nome utente (CAMPO OBBLIGATORIO)");
-			nome ="'"+ scan.nextLine()+"'";
+			nome ="'"+ scan.nextLine().trim()+"'";
 		}
 		
 		
 		String cognome=null;
-		System.out.println("Cognome utente ");
-		cognome ="'"+ scan.nextLine()+"'";
+		System.out.println("Cognome utente "); 			//come nome
+		cognome ="'"+ scan.nextLine().trim()+"'";
 		
 		while (cognome.length()==2) {
 			System.out.println("Cognome utente (CAMPO OBBLIGATORIO)");
-			cognome = "'"+scan.nextLine()+"'";
+			cognome = "'"+scan.nextLine().trim()+"'";
 		}
 		
-		System.out.println("Mail utente (INVIO PER SALTARE)");
-		String a = scan.nextLine();
-		String mail = a.length()!=0? "'"+a+"'": null;
+		System.out.println("Mail utente (INVIO PER SALTARE)");		
+		String a = scan.nextLine();						//indrizzo mail, se vuoto allora 
+		String mail = a.length()!=0? "'"+a+"'": null;	//viene inizializzato a null, altrimenti viene messo tra apici
 		
 		System.out.println("Mail lavoro utente (INVIO PER SALTARE)");
-		String b = scan.nextLine();
+		String b = scan.nextLine();						//come mail
 		String workMail = b.length()!=0? "'"+b+"'": null;
 		
 		System.out.println("Telefono utente (INVIO PER SALTARE)");
-		String c = scan.nextLine();
+		String c = scan.nextLine();						//come mail
 		String phone = c.length()!=0? "'"+c+"'": null;
 		
 		String cf;
-		do {
+		do {							
 			System.out.println("Codice Fiscale utente (16 cifre, CAMPO OBBLIGATORIO)");
 			cf="'"+ scan.nextLine()+"'";
 		}
-		while (cf.length() != 18);
+		while (cf.length() != 18);		//il codice fiscale avrà lunghezza 18 perchè aggiunti gli apici ('')
 		
 		System.out.println("Status utente (Yes per admin) CAMPO OBBLIGATORIO");
-		int ad =scan.nextLine().equalsIgnoreCase("yes")? 1 : 0;
-		
+		int ad =scan.nextLine().equalsIgnoreCase("yes")? 1 : 0;		//SQL prende come valore booleano un bit
+																	//1 vero, 0 falso
 		System.out.println("Password utente (INVIO PER SALTARE)");
-		String d = scan.nextLine();
+		String d = scan.nextLine();				//come mail
 		String pw = d.length()!=0? "'"+d+"'": null;
-		
-		
+
+
 		query="INSERT INTO user VALUES (null, "+ nome +", "+cognome+", " +mail +", " 
-				+workMail+", "+phone+", " +cf+", "+ad+", "+pw +")";
+				+workMail+", "+phone+", " +cf+", "+ad+", "+pw +")";    //creazione della query
 		return query;
-			
+
 	}
 
-	public static String modificaUtente(String idUtente) {
+	public static String  modificaUtente(String idUtente) {
+		String campo, nome, cognome, p_email, w_email, cell, cf, pass;  
 
-		String query=null;
-		
-		
-		return query;
-		
+		StringBuilder creaQuery = new StringBuilder("UPDATE user set ");
+
+		System.out.println("Cosa vuoi modificare? first_name");
+		System.out.println("first_name, last_name, personal_email, work_email, phone, fiscal_code, password");
+		campo = scan.nextLine().toLowerCase();
+		//modifica il campo scelto dall'utente
+		switch(campo) {
+		case "first_name":
+			nome = prendiCampo(campo);
+			creaQuery.append(campo +"="+ "'"+ nome +"'" +",");		//sintassi SQL, come la creazione
+			break;
+		case "last_name":
+			cognome = prendiCampo(campo);
+			creaQuery.append(campo +"="+ "'"+cognome+"'"  +",");
+			break;
+		case "personal_email":
+			p_email = prendiCampo(campo);
+			creaQuery.append(p_email.length()==0?campo +"="+ null +",":campo +"="+ "'"+p_email +"'" +",");
+//			if (p_email.length()==0) {
+//				creaQuery.append(campo +"="+ null +",");   //in questo modo, se il valore viene messo a null non avremo
+//			}												// una stringa vuota
+//			else {
+//			creaQuery.append(campo +"="+ "'"+p_email +"'" +",");
+//			}
+			break;
+		case "work_email":
+			w_email = prendiCampo(campo);
+			if (w_email.length()==0) {						//vedi sopra 
+				creaQuery.append(campo +"="+ null +",");
+			}
+			else {
+			creaQuery.append(campo +"="+ "'"+w_email +"'" +",");
+			}
+			break;
+		case "phone":
+			cell=prendiCampo(campo);
+			if (cell.length()==0) {
+				creaQuery.append(campo +"="+ null +",");			//guarda sù
+			}
+			else {
+			creaQuery.append(campo +"="+ "'"+cell +"'" +",");
+			}
+			break;
+		case "fiscal_code":
+			cf = prendiCampo(campo);
+			creaQuery.append(campo +"="+ "'"+cf +"'" +",");
+			break;
+		case "password":
+			pass = prendiCampo(campo);
+			if (pass.length()==0) {					//come mail
+				creaQuery.append(campo +"="+ null +",");
+			}
+			else {
+			creaQuery.append(campo +"="+ "'"+pass +"'" +",");
+			}
+			break;
+		default:
+			System.out.println("Input non valido");
+			return null;
+		}
+		System.out.println(creaQuery+"prima di charat");
+		creaQuery.deleteCharAt(creaQuery.length()-1);    	
+		System.out.println(creaQuery);
+		creaQuery.append(" where id = "+ idUtente);
+
+		scan.close();
+
+		return creaQuery.toString();
+
+	}
+
+	public static String prendiCampo(String c) {
+		String nuovoValore;
+
+		System.out.println("Inserisci il nuovo " + c);
+		nuovoValore = scan.nextLine().trim();
+
+		return nuovoValore;
 	}
 
 	public static String eliminaUtente(String idUtente) {
-
 		String query=null;
-		
-		
+		query="DELETE FROM user WHERE id = " + idUtente;
 		return query;
 	}
 
