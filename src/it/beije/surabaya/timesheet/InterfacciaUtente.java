d8e0a0c8ea56056f693ef9ea1a7616c6d6593e62
davidezacchino <davidezacchino@192.168.1.7>
2019-04-25 14:54:53 +0200

    Database ImportExport e implementazione nell'interfaccia Utente

---
+++ src/it/beije/surabaya/timesheet/InterfacciaUtente.java
@@ -0,0 +1,176 @@
+package it.beije.surabaya.timesheet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+//import java.util.ArrayList;
+//import java.util.List;
+
+//import chapter4.CSVutils;
+
+public class InterfacciaUtente {
+
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "Beije04";
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/beije?serverTimezone=CET";
+	
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		Connection conn = null;
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+		
+		return conn;
+	}
+	
+	public static void main(String[] args) {
+		
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rset= null;
+		
+		try {
+			conn = getConnection();
+			
+			stmt = conn.createStatement();
+			
+			//utenti SELECT
+			
+			Scanner scan=new Scanner(System.in);
+			System.out.println("Cosa vuoi fare? c-Crea, m-Modifica, e-Elimina, i-Importa CSV, ex1-Esporta CSV, ex2-Esporta XML");
+			
+			String scelta=scan.nextLine().toLowerCase();
+			String idUtente;
+			String query = null;
+			String[] query_array = null;
+			
+			switch (scelta) {
+			case "c":
+				query = TSutils.creaUtente();
+				String[] campi = query.split(" ");
+				String cf = campi[10].substring(1, campi[10].length()-2);
+				rset = stmt.executeQuery("SELECT * FROM user WHERE fiscal_code="+cf);
+				if(rset.first()) {
+					System.out.println("Questo codice fiscale esiste già");
+					query = null;
+				}
+				break;
+			case "m":
+				System.out.println("ID da modificare:");				
+				idUtente = scan.nextLine();
+				query = TSutils.modificaUtente(idUtente);
+				//System.out.println(query);
+				if(query.indexOf("fiscal_code=")!=-1) {
+					cf = query.substring(29, 45);
+					System.out.println(cf);
+					rset = stmt.executeQuery("SELECT * FROM user WHERE fiscal_code='"+cf+"'");
+					if(rset.first()) {
+						System.out.println("Questo codice fiscale esiste già");
+						query = null;
+					}
+				}
+				//System.out.println(query.substring(29, 45));
+				break;
+			case "e":
+				System.out.println("ID da eliminare:");
+				idUtente = scan.nextLine();
+				rset = stmt.executeQuery("SELECT * FROM timetable WHERE id_user="+idUtente);
+				if(rset.first()) {
+					System.out.println("Questo utente ha delle tabelle orario associate, "
+							+ "sicuro di volerle cancellare? YES per confermare");
+					String sicurezza = scan.nextLine();
+					if(sicurezza.equalsIgnoreCase("yes")){
+						stmt.execute("DELETE FROM timetable WHERE id_user="+idUtente);
+					}else {
+						break;
+					}
+				}
+				query = TSutils.eliminaUtente(idUtente);
+				break;
+			case "i":
+				{
+					System.out.println("Inserisci path file CSV da importare: ");
+					String path = scan.nextLine();
+					query_array = DBImportExport.importCsv(path);
+				}
+				break;
+			case "ex1":
+				{
+					System.out.println("Inserisci path file CSV da esportare: ");
+					String path = scan.nextLine();
+					DBImportExport.exportCsv(path, conn, stmt);
+				}
+				break;
+			case "ex2":
+				{
+					System.out.println("Inserisci path file CSV da esportare: ");
+					String path = scan.nextLine();
+					DBImportExport.exportXml(path, conn, stmt);
+				}
+				break;
+			default: 
+				System.out.println("Scelta sbagliata");
+			}
+			
+			if(query!=null) {
+				stmt.execute(query);
+			}
+			
+			if(query_array!=null) {
+				for(String q : query_array)
+				stmt.execute(q);
+			}
+			
+			scan.close();
+			
+//			String query2 = "SELECT * FROM user";
+//			rset = stmt.executeQuery(query2);
+//			
+//			List<String> utenti = new ArrayList<>();
+//			utenti.add("Cognome, Nome, Numero, Email");
+//			
+//			while (rset.next()) {
+//				
+//				String nome = rset.getString("first_name");
+//				String cognome= rset.getString("last_name");
+//				String mail = rset.getString("personal_email");
+//				String workMail= rset.getString("work_email");
+//				String phone=rset.getString("phone");
+//				String cf=rset.getString("fiscal_code");
+//				String ad=rset.getString("admin");
+//				String pw=rset.getString("password");
+//				utenti.add("" + nome+", " + cognome+", "+mail+", "+workMail+", "+phone+", "+cf+", "+ad+", "+pw);
+//			
+//			}
+//			String[] arrayUtenti;
+//			arrayUtenti = utenti.toArray(new String[utenti.size()]);
+//			for(String utente:utenti) {
+//				System.out.println(utente);
+//			}
+			//CSVutils.writeRowsInFile("C:\\Users\\Padawan04\\Desktop\\Marco\\nomiCognomi\\rubrica_random.csv", arrayUtenti);
+			
+		}catch (SQLException se) {
+			System.out.println("SQLError: " + se.getMessage() + " code: " + se.getErrorCode());
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		finally {
+			try {
+//				rset.close();
+				stmt.close();
+				conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ src/it/beije/surabaya/timesheet/TSutils.java
@@ -0,0 +1,206 @@
+package it.beije.surabaya.timesheet;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+public class TSutils {
+	static Scanner scan = new Scanner(System.in);
+	public static String creaUtente() {
+		String query=null;
+		
+		
+		
+		String nome=null;
+		System.out.println("Nome utente ");
+		nome ="'"+ scan.nextLine().trim()+"'";			//Formato per query SQL
+		
+		while (nome.length()==2) {						//Se il nome è null, allora resteranno solo gli apici ('')
+			System.out.println("Nome utente (CAMPO OBBLIGATORIO)");
+			nome ="'"+ scan.nextLine().trim()+"'";
+		}
+		
+		
+		String cognome=null;
+		System.out.println("Cognome utente "); 			//come nome
+		cognome ="'"+ scan.nextLine().trim()+"'";
+		
+		while (cognome.length()==2) {
+			System.out.println("Cognome utente (CAMPO OBBLIGATORIO)");
+			cognome = "'"+scan.nextLine().trim()+"'";
+		}
+		
+		System.out.println("Mail utente (INVIO PER SALTARE)");		
+		String a = scan.nextLine();						//indrizzo mail, se vuoto allora 
+		String mail = a.length()!=0? "'"+a+"'": null;	//viene inizializzato a null, altrimenti viene messo tra apici
+		
+		System.out.println("Mail lavoro utente (INVIO PER SALTARE)");
+		String b = scan.nextLine();						//come mail
+		String workMail = b.length()!=0? "'"+b+"'": null;
+		
+		System.out.println("Telefono utente (INVIO PER SALTARE)");
+		String c = scan.nextLine();						//come mail
+		String phone = c.length()!=0? "'"+c+"'": null;
+		
+		String cf;
+		do {							
+			System.out.println("Codice Fiscale utente (16 cifre, CAMPO OBBLIGATORIO)");
+			cf="'"+ scan.nextLine()+"'";
+		}
+		while (cf.length() != 18);		//il codice fiscale avrà lunghezza 18 perchè aggiunti gli apici ('')
+		
+		System.out.println("Status utente (Yes per admin) CAMPO OBBLIGATORIO");
+		int ad =scan.nextLine().equalsIgnoreCase("yes")? 1 : 0;		//SQL prende come valore booleano un bit
+																	//1 vero, 0 falso
+		System.out.println("Password utente (INVIO PER SALTARE)");
+		String d = scan.nextLine();				//come mail
+		String pw = d.length()!=0? "'"+d+"'": null;
+
+
+		query="INSERT INTO user VALUES (null, "+ nome +", "+cognome+", " +mail +", " 
+				+workMail+", "+phone+", " +cf+", "+ad+", "+pw +")";    //creazione della query
+		return query;
+
+	}
+
+	public static String  modificaUtente(String idUtente) {
+		String campo, nome, cognome, p_email, w_email, cell, cf, pass;  
+
+		StringBuilder creaQuery = new StringBuilder("UPDATE user set ");
+
+		System.out.println("Cosa vuoi modificare? first_name");
+		System.out.println("first_name, last_name, personal_email, work_email, phone, fiscal_code, password");
+		campo = scan.nextLine().toLowerCase();
+		//modifica il campo scelto dall'utente
+		switch(campo) {
+		case "first_name":
+			nome = prendiCampo(campo);
+			creaQuery.append(campo +"="+ "'"+ nome +"'" +",");		//sintassi SQL, come la creazione
+			break;
+		case "last_name":
+			cognome = prendiCampo(campo);
+			creaQuery.append(campo +"="+ "'"+cognome+"'"  +",");
+			break;
+		case "personal_email":
+			p_email = prendiCampo(campo);
+			creaQuery.append(p_email.length()==0?campo +"="+ null +",":campo +"="+ "'"+p_email +"'" +",");
+//			if (p_email.length()==0) {
+//				creaQuery.append(campo +"="+ null +",");   //in questo modo, se il valore viene messo a null non avremo
+//			}												// una stringa vuota
+//			else {
+//			creaQuery.append(campo +"="+ "'"+p_email +"'" +",");
+//			}
+			break;
+		case "work_email":
+			w_email = prendiCampo(campo);
+			if (w_email.length()==0) {						//vedi sopra 
+				creaQuery.append(campo +"="+ null +",");
+			}
+			else {
+			creaQuery.append(campo +"="+ "'"+w_email +"'" +",");
+			}
+			break;
+		case "phone":
+			cell=prendiCampo(campo);
+			if (cell.length()==0) {
+				creaQuery.append(campo +"="+ null +",");			//guarda sù
+			}
+			else {
+			creaQuery.append(campo +"="+ "'"+cell +"'" +",");
+			}
+			break;
+		case "fiscal_code":
+			do {							
+				System.out.println("Codice Fiscale utente (16 cifre, CAMPO OBBLIGATORIO)");
+				cf = prendiCampo(campo);
+			}
+			while (cf.length()!= 16);
+			creaQuery.append(campo +"="+ "'"+cf +"'" +",");
+			break;
+		case "password":
+			pass = prendiCampo(campo);
+			if (pass.length()==0) {					//come mail
+				creaQuery.append(campo +"="+ null +",");
+			}
+			else {
+			creaQuery.append(campo +"="+ "'"+pass +"'" +",");
+			}
+			break;
+		default:
+			System.out.println("Input non valido");
+			return null;
+		}
+		
+		creaQuery.deleteCharAt(creaQuery.length()-1);    	//in questo modo toglie l'ultimo carattere che è la virgola
+		
+		creaQuery.append(" where id = "+ idUtente);			// finisce di creare la query specificando l'id
+
+		scan.close();
+
+		return creaQuery.toString();
+
+	}
+
+	public static String prendiCampo(String c) {			//metodo per inserire il valore del campo modificato
+		String nuovoValore;
+
+		System.out.println("Inserisci il nuovo " + c);
+		nuovoValore = scan.nextLine().trim();
+
+		return nuovoValore;
+	}
+
+	public static String eliminaUtente(String idUtente) {  //eliminazione dell'utente
+		String query=null;
+		query="DELETE FROM user WHERE id = " + idUtente;    //creazione della query per eliminare
+		return query;
+	}
+
+	public static String inserisciRigaLavorativa(String idUtente) {
+
+		System.out.println("Inserisci la data: yyyy-MM-dd");
+		String dataInput = scan.nextLine();
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate data = LocalDate.parse(dataInput, f);
+		System.out.println(data);
+		return null;
+	}
+
+	public static String inserisciRigaPermesso(String idUtente) {
+
+		return null;		
+	}
+
+	public static String inserisciRigaFerie(String idUtente) {
+
+		return null;
+	}
+
+	public static String inserisciRigaMalattia(String idUtente) {
+		return null;
+		
+	}
+
+	public static String modificaRigaLavorativa(String idUtente) {
+		return null;
+		
+	}
+
+	public static String modificaRigaPermesso(String idUtente) {
+		return null;
+		
+	}
+
+	public static String modificaRigaFerie(String idUtente) {
+		return null;
+		
+	}
+
+	public static String modificaRigaMalattia(String idUtente) {
+		return null;
+		
+	}
+	
+	
+
+}
