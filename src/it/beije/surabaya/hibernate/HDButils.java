7bb56538882057c5f88212a0d60437bcfdf9f847
ivfly <ivo.mosca@privategriffe.com>
2019-04-30 12:57:25 +0200

    hibernate no annotations

--- src/it/beije/surabaya/hibernate/HDButils.java
+++ src/it/beije/surabaya/hibernate/HDButils.java
@@ -1,7 +1,18 @@
 package it.beije.surabaya.hibernate;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.query.Query;
+
+import it.beije.surabaya.hibernate.entities.Utente;
 
 
 public class HDButils {
@@ -70,6 +81,41 @@ public class HDButils {
 		
 		System.out.println("is open?" + factory.isOpen());
 		
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+//		String hql = "SELECT id FROM Utente";
+//		Query query = session.createQuery(hql);
+//		for (Object u : query.list()) {
+//			Long utente = (Long) u;
+//			System.out.println(utente);
+//		}
+		
+		Criteria criteria = session.createCriteria(Utente.class);
+		List utenti = criteria.list();//.add(Restrictions.eq("nome", "pippo"))
+
+		Iterator itr = utenti.iterator();
+		while (itr.hasNext()) {
+//			Transaction transaction = session.beginTransaction();
+			Utente u = (Utente) itr.next();
+			System.out.println(u.getId());
+			System.out.println(u.getCognome());		
+			
+			StringBuilder b = new StringBuilder();
+			for (int i = 0; i < (u.getId()*5); i++) {
+				b.append('c');
+			}
+			u.setNome(b.toString());
+			System.out.println("new name : " + b.toString());
+//			transaction.commit();
+		}
+		
+		
+		//System.out.println(query.list());
+
+
+		//transaction.commit();
+		session.close();
 		factory.close();
 		System.out.println("is open?" + factory.isOpen());
 	}
